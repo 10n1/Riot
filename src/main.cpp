11bb4974c75e76b0c1f07a5419af4d13d1b89398
Kyle Weicht <kweicht@gmail.com>
2011-07-07 14:51:31 -0700

    More changes

--- src/main.cpp
+++ src/main.cpp
@@ -42,17 +42,54 @@ Objects
 class Entity
 {
     Transform   object_transform;
-    Component   components[100];
+    Component   components[];
     int         num_components;
     
     void AddComponent( ComponentType type );
     void RemoveComponent( ComponentType type );
     Component GetComponent( ComponentType type );
+    Transform GetTransform( void );
+    void SetTransform( Transform new_transform ); // Should probably buffer the transform, but thats irrelevant now
 }
 
 class Component
 {
     Entity* owner_entity;
+}
+
+//-------------------------------------------------------
+class ExampleMeshRenderComponent
+{
+    Mesh    mesh_to_render;
+    
+    void ProcessComponent( void )
+    {
+        Renderer->SetTransform( owner_entity->GetTransform() );
+        Renderer->RenderMesh( mesh_to_render );
+    }
+}
+
+class ExampleCollidableComponent
+{
+    Shape   collision_shape;
+}
+
+class ExampleRigidBody
+{
+    float3  velocity;
+    float3  acceleration;
+    float   mass;
+    
+    void ProcessComponent( void )
+    {
+        ExampleCollidableComponent collision_body = owner_entity->GetComponent( ExampleCollidableComponent );
+        
+        if( collision_body == nullptr ) return;
+        
+        PerformPhysics( collision_body );
+        Transform new_transform = UpdateObject( owner_entity->GetTransform(), velocity, acceleration, mass );
+        owner_entity->SetTransform( new_transform );
+    }
 }
 
 /*    Object Creation
@@ -71,3 +108,51 @@ can still continue exectution. This might mean it just returns, but thats okay, 
 
 Some components don't do anything (A collidable component) while others do stuff every frame (particle
 emitters).
+*/
+
+/*    Behavior location
+Can be a component:
+    Audio emitter
+    Particle emitter
+    Particle renderer
+    Mesh renderer
+    Collidable
+    Rigid body
+    Gameplay trigger
+    Controller
+        AI controller
+        User input (for controlling entities [aka a character])
+    Debug visualizations
+    Inventory system
+    Camera
+ 
+Not component based:
+    Renderer
+    Tasking system
+    Networking
+    File IO
+    Input system
+    Terrain
+    UI system
+    Picking
+*/
+
+/*    Subsystems
+*/
+class GraphicsDevice
+{
+    void*   system_gfx_device; // ID3D11Device, CGLContextObj, etc
+    
+    void SetVertexBuffer( VertexBuffer buffer ); 
+    ...
+}
+
+class Renderer
+{
+    GraphicsDevice  graphics_device;
+    RenderCommand   render_commands;
+    
+    void AddRenderCommand( RenderCommand command );
+    void SortCommands( void );
+    void Render( void );
+}
