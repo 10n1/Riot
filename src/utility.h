5885d86e76f52d9a4fb75d60df241933d721ccef
Kyle Weicht <kweicht@gmail.com>
2011-12-16 23:39:37 -0800

    Nice and clean. Good first start

--- src/utility.h
+++
@@ -1,9 +0,0 @@
-//
-//  utility.h
-//  Riot
-//
-//  Created by Kyle C Weicht on 7/12/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
-
-#define MALLOC_STRUCT( Struct ) (Struct*)malloc(sizeof(Struct))
---
+++ test/main.cpp
@@ -0,0 +1,17 @@
+/*
+ *  main.cpp
+ *  Riot test
+ *
+ *  Created by Kyle Weicht on 12/16/11.
+ *  Copyright (c) 2011 Kyle Weicht. All rights reserved.
+ */
+#include <cstdio>
+
+/*******************************************************************\
+ External functions
+\*******************************************************************/
+int main(int argc, char* argv[])
+{
+    printf("Test!");
+    return 0;
+}
