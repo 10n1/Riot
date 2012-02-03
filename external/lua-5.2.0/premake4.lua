83d1dbda791e8541bcf050ffec08a49c4d9a9ca6
Kyle Weicht <kweicht@gmail.com>
2012-02-02 18:12:47 -0800

    Lua premake

---
+++ external/lua-5.2.0/premake4.lua
@@ -0,0 +1,24 @@
+-- Lua script
+
+local action = _ACTION or ""
+
+solution "Lua"
+	location ( "Build/" .. action )
+	configurations { "Debug", "Release" }
+	platforms { "x32", "x64" }
+	
+	configuration "Debug"
+		targetdir ( "Build/" .. action .. "/bin/Debug" )
+		flags { "Symbols" }
+
+   configuration "Release"
+		targetdir ( "Build/" .. action .. "/bin/Release" )
+		defines { "NDEBUG" }
+		flags { "Optimize" }
+
+	project "Lua"
+		kind "StaticLib"
+		language "C"
+		files { "src/**.h", "src/**.cpp" }
+		excludes { "src/lua.c", "src/luac.c" }
+		includedirs { "src" }
