8bd7c67f300987df3174bf8f5051bbc70db3f050
Kyle Weicht <kweicht@gmail.com>
2012-02-02 23:07:21 -0800

    Project definition

--- projects/premake4.lua
+++ projects/premake4.lua
@@ -7,19 +7,45 @@ solution "Riot"
     platforms { "x64" }
     targetdir("../build/bin");
     flags{ "Symbols", "NoMinimalRebuild", "ExtraWarnings", "NoExceptions", "NoPCH", "NoRTTI" }
+    
     configuration "windows"
         buildoptions { "/MP" }
-    
+
     project "RiotLib"
         kind        "StaticLib"
         language    "C++"
         files { "../lib/**.*" }
-        includedirs { "../include" }
+        includedirs { "../include", "../external/Box2D_v2.2.1", "../external/lua-5.2.0/src" }
         objdir { "../build/obj/lib" }
-        
+        links { "Box2D", "lua" }
+
         configuration "not macosx"
-            excludes { "../**.m", "../**_macosx**"}
+            excludes { "../**.m", "../**macosx**"}
         configuration "not windows"
-            excludes { "../**_win32**" }
-        configuration "windows"
-            buildoptions { "/MP" }
+            excludes { "../**win32**" }
+        
+        configuration { "windows", "Debug" }
+            libdirs { "../external/Box2D_v2.2.1/Build/vs2010/bin/Debug", "../external/lua-5.2.0/Build/vs2010/bin/Debug" }
+        configuration { "windows", "Release" }
+            libdirs { "../external/Box2D_v2.2.1/Build/vs2010/bin/Release", "../external/lua-5.2.0/Build/vs2010/bin/Release" }
+            
+    project "RiotGame"
+        kind        "ConsoleApp"
+        language    "C++"
+        files { "../game/**.*" }
+        includedirs { "../include" }
+        objdir { "../build/obj/game" }
+        links { "RiotLib", "Box2D", "lua" }
+        
+        configuration { "windows", "Debug" }
+            libdirs { "../external/Box2D_v2.2.1/Build/vs2010/bin/Debug", "../external/lua-5.2.0/Build/vs2010/bin/Debug" }
+        configuration { "windows", "Release" }
+            libdirs { "../external/Box2D_v2.2.1/Build/vs2010/bin/Release", "../external/lua-5.2.0/Build/vs2010/bin/Release" }
+            
+    project "RiotTest"
+        kind        "ConsoleApp"
+        language    "C++"
+        files { "../test/**.*" }
+        includedirs { "../include" }
+        objdir { "../build/obj/test" }
+        links { "RiotLib" }
